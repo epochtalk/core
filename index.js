1e2d2db8eb704494155e6a5ee84603530eaf058b
James Wang <jw@slickage.com>
2014-06-26 15:33:05 -1000

    Levelup module and temporary code.

---
+++ index.js
@@ -0,0 +1,11 @@
+var levelup = require('levelup')
+var db = levelup(__dirname + '/epochcore_db')
+
+// temporary
+db.put('name', 'epochcore', function (err) {
+  if (err) return console.log('Ooops!', err) // some kind of I/O error
+  db.get('name', function (err, value) {
+    if (err) return console.log('Ooops!', err) // likely the key was not found
+    console.log('name=' + value)
+  })
+})
