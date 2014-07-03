0d72ab3cf7ca5233120db5114c50f84bb6f42854
James Wang <jw@slickage.com>
2014-07-02 19:18:25 -1000

    Querying test with only id's on each line in printout.

--- test/query_posts.js
+++ test/query_posts.js
@@ -2,11 +2,8 @@ var posts = require(__dirname + '/../posts');
 
 posts.all(function(err, results) {
   results.forEach(function(post) {
-    console.log(post);
     posts.find(post.value.id, function(err, post) {
-      console.log('--- retrieved:');
-      console.log(post);
+      console.log(post.id);
     });
   });
-  console.log(results.length);
 });
