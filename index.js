23c1d2a2e5d8133ce902f7a481a78dd4173ba335
Anthony Kinsey <anthony@slickage.com>
2014-08-04 11:18:03 -1000

    Added new threads model to core exports

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var path = require('path');
-var levelup = require('levelup');
 var core = {};
 core.users = require(path.join(__dirname, 'users'));
 core.boards = require(path.join(__dirname, 'boards', 'boards'));
 core.posts = require(path.join(__dirname, 'posts', 'posts'));
+core.threads = require(path.join(__dirname, 'threads', 'threads'));
 
 module.exports = core;
