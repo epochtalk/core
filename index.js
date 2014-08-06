3e4f799e91e4a3c5676b8200ea9b1590cda86b33
James Wang <jw@slickage.com>
2014-08-06 11:09:39 -1000

    Refactoring models to model/index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var path = require('path');
 var core = {};
 core.users = require(path.join(__dirname, 'users'));
-core.boards = require(path.join(__dirname, 'boards', 'boards'));
-core.posts = require(path.join(__dirname, 'posts', 'posts'));
-core.threads = require(path.join(__dirname, 'threads', 'threads'));
+core.boards = require(path.join(__dirname, 'boards'));
+core.posts = require(path.join(__dirname, 'posts'));
+core.threads = require(path.join(__dirname, 'threads'));
 
 module.exports = core;
