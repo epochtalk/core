935c71af0891b264766410272c6cc1409890c7cd
Anthony Kinsey <anthony@slickage.com>
2014-08-07 18:03:36 -1000

    Added benchmarkjs as dev dependency. Updated benchmark tests
    * Added delete to benchmark tests
    * Keeping track of ids upon creation benchmark to pass to deletion
      benchmark. Looking the ids up out of an array might slightly affect
      the outcome of the benchmark, but it can't be helped.
    * Indexes are not being deleted, also all posts/boards/threads are not
      all deleted because we arent specifying an exact sample size for each
      benchmark test

--- seed/benchmark.js
+++ seed/benchmark.js
@@ -1,9 +1,16 @@
 var path = require('path');
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
-var boards = require(path.join(__dirname, '..', 'boards', 'boards'));
-var threads = require(path.join(__dirname, '..', 'threads', 'threads'));
-var posts = require(path.join(__dirname, '..', 'posts', 'posts'));
+var boards = require(path.join(__dirname, '..', 'boards'));
+var threads = require(path.join(__dirname, '..', 'threads'));
+var posts = require(path.join(__dirname, '..', 'posts'));
+var threadIds = [];
+var boardIds = [];
+var postIds = [];
+var threadIndex = 0;
+var boardIndex = 0;
+var boardDeleteIndex = 0;
+var postDeleteIndex = 0;
 
 suite.add('Boards#Create', {
   defer: true,
@@ -12,7 +19,10 @@ suite.add('Boards#Create', {
       name: 'Board',
       description: 'Hello World! This is a board in a popular forum.'
     },
-    function() {
+    function(err, board) {
+      if (!err) {
+        boardIds.push(board.id);
+      }
       deferred.resolve();
     });
   }
@@ -23,8 +33,12 @@ suite.add('Boards#Create', {
     threads.create({
       title: 'Thread',
       body: 'Hello World! This is a thread in a board on a popular forum.',
-      board_id: 'boards~abcdefghijklmnopqrstuvwxyz'},
-    function() {
+      board_id: boardIds[boardIndex++]
+    },
+    function(err, thread) {
+      if (!err) {
+        threadIds.push(thread.thread_id);
+      }
       deferred.resolve();
     });
   }
@@ -35,9 +49,33 @@ suite.add('Boards#Create', {
     posts.create({
       title: 'Post',
       body: 'Hello World! This is a post in a thread on a popular forum.',
-      thread_id: 'threads~abcdefghijklmnopqrstuvwxyz'},
-    function() {
+      thread_id: threadIds[threadIndex++]
+    },
+    function(err, post) {
+      if (!err) {
+        postIds.push(post.id);
+      }
       deferred.resolve();
+    });
+  }
+})
+.add('Boards#Delete', {
+  defer: true,
+  fn: function(deferred) {
+    boards.delete(boardIds[boardDeleteIndex++], function(err) {
+      if (!err) {
+        deferred.resolve();
+      }
+    });
+  }
+})
+.add('Posts#Delete', {
+  defer: true,
+  fn: function(deferred) {
+    posts.delete(postIds[postDeleteIndex++], function(err) {
+      if (!err) {
+        deferred.resolve();
+      }
     });
   }
 })
