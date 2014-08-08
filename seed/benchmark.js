500661c05eab4f419c2fa1a4eff74bbf32b0e5bb
Anthony Kinsey <anthony@slickage.com>
2014-08-07 18:32:42 -1000

    Added Update benchmarks for boards, threads and posts

--- seed/benchmark.js
+++ seed/benchmark.js
@@ -5,11 +5,15 @@ var boards = require(path.join(__dirname, '..', 'boards'));
 var threads = require(path.join(__dirname, '..', 'threads'));
 var posts = require(path.join(__dirname, '..', 'posts'));
 var threadIds = [];
+var threadIndexIds = [];
 var boardIds = [];
 var postIds = [];
 var threadIndex = 0;
+var threadUpdateIndex = 0;
 var boardIndex = 0;
+var boardUpdateIndex = 0;
 var boardDeleteIndex = 0;
+var postUpdateIndex = 0;
 var postDeleteIndex = 0;
 
 suite.add('Boards#Create', {
@@ -27,6 +31,19 @@ suite.add('Boards#Create', {
     });
   }
 })
+.add('Boards#Update', {
+  defer: true,
+  fn: function(deferred) {
+    boards.update({
+      name: 'Board Update!',
+      description: 'Updated! Hello World! This is a board in a popular forum.',
+      id: boardIds[boardUpdateIndex++]
+    },
+    function() {
+      deferred.resolve();
+    });
+  }
+})
 .add('Threads#Create', {
   defer: true,
   fn: function(deferred) {
@@ -34,6 +51,22 @@ suite.add('Boards#Create', {
       title: 'Thread',
       body: 'Hello World! This is a thread in a board on a popular forum.',
       board_id: boardIds[boardIndex++]
+    },
+    function(err, thread) {
+      if (!err) {
+        threadIds.push(thread.thread_id);
+        threadIndexIds.push(thread.id);
+      }
+      deferred.resolve();
+    });
+  }
+})
+.add('Threads#Update', {
+  defer: true,
+  fn: function(deferred) {
+    threads.update({
+      post_count: 99,
+      id: threadIndexIds[threadUpdateIndex++],
     },
     function(err, thread) {
       if (!err) {
@@ -59,13 +92,16 @@ suite.add('Boards#Create', {
     });
   }
 })
-.add('Boards#Delete', {
+.add('Posts#Update', {
   defer: true,
   fn: function(deferred) {
-    boards.delete(boardIds[boardDeleteIndex++], function(err) {
-      if (!err) {
-        deferred.resolve();
-      }
+    posts.update({
+      title: 'Post Updated!',
+      body: 'Updated! Hello World! This is a post in a thread on a popular forum.',
+      id: postIds[postUpdateIndex++]
+    },
+    function() {
+      deferred.resolve();
     });
   }
 })
@@ -73,6 +109,16 @@ suite.add('Boards#Create', {
   defer: true,
   fn: function(deferred) {
     posts.delete(postIds[postDeleteIndex++], function(err) {
+      if (!err) {
+        deferred.resolve();
+      }
+    });
+  }
+})
+.add('Boards#Delete', {
+  defer: true,
+  fn: function(deferred) {
+    boards.delete(boardIds[boardDeleteIndex++], function(err) {
       if (!err) {
         deferred.resolve();
       }
