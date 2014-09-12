fab04448df3bf2a004d3dc3aeaf639d272d3c5e3
James Wang <jw@slickage.com>
2014-09-11 23:29:10 -1000

    Sublevel refactor wip.

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ module.exports = db;
 
 var path = require('path');
 var levelup = require('levelup');
+var sublevel = require('level-sublevel');
 var Promise = require('bluebird');
 var mkdirp = require('mkdirp');
 var config = require(path.join(__dirname, 'config'));
@@ -13,12 +14,14 @@ var dbPath = config.dbPath;
 var jsonEncoding = {valueEncoding: 'json'};
 var utfEncoding = {valueEncoding: 'utf8'};
 
-var content = levelup(path.join(dbPath, 'content'), jsonEncoding);
-var messages = levelup(path.join(dbPath, 'messages'), jsonEncoding);
-var deleted = levelup(path.join(dbPath, 'deleted'), jsonEncoding);
-var metadata = levelup(path.join(dbPath, 'metadata'), utfEncoding);
-var indexes = levelup(path.join(dbPath, 'indexes'), utfEncoding);
-var legacy = levelup(path.join(dbPath, 'legacy'), utfEncoding);
+var db = sublevel(levelup(dbPath));
+
+var content = db.sublevel('content');
+var messages = db.sublevel('messages');
+var deleted = db.sublevel('deleted');
+var metadata = db.sublevel('metadata');
+var indexes = db.sublevel('indexes');
+var legacy = db.sublevel('legacy');
 
 db.content = Promise.promisifyAll(content);
 db.messages = Promise.promisifyAll(messages);
--- index.js
+++ index.js
@@ -7,11 +7,10 @@ module.exports = function(dbPath) {
   if (dbPath) {
     config.dbPath = dbPath;
   }
-
   core.users = require(path.join(__dirname, 'users'));
   core.boards = require(path.join(__dirname, 'boards'));
   core.posts = require(path.join(__dirname, 'posts'));
   core.threads = require(path.join(__dirname, 'threads'));
-
   return core;
 };
+
