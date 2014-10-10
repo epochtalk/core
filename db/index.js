fff252237cdeca53949feb95392902d291f24a58
James Wang <jw@slickage.com>
2014-10-09 17:52:02 -1000

    Encoding now defined in treedb.

--- db/index.js
+++ db/index.js
@@ -14,7 +14,7 @@ var dbPath = config.dbPath;
 var jsonEncoding = {valueEncoding: 'json'};
 var utfEncoding = {valueEncoding: 'utf8'};
 
-var db = new TreeDB(levelup(path.join(dbPath), jsonEncoding)).db;
+var db = new TreeDB(levelup(path.join(dbPath))).db;
 var messages = db.sublevel('messages');
 var deleted = db.sublevel('deleted');
 var metadata = db.sublevel('metadata');
