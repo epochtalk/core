93a3004b74b07fb8c78239ba5d25ba1801905926
Edward Kim <ed@slickage.com>
2014-10-31 16:41:44 -1000

    Fixed an issue with parse user.signature
    The user signature parser will always try to parse the user's signature.
    This commit now check to see if the user signature exists first. Then
    try to parse it.

--- users/pre.js
+++ users/pre.js
@@ -5,7 +5,7 @@ var bbcodeParser = require('bbcode-parser');
 
 users.parseSignature = function(user) {
   // check if signature has any bbcode
-  if (user.signature.indexOf('[') >= 0) {
+  if (user.signature && user.signature.indexOf('[') >= 0) {
     // parse encodedBody to generate body
     var parsed = bbcodeParser.process({text: user.signature}).html;
     user.signature = parsed;
