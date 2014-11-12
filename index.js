e982fdf3e2e9865aaa33750fb32ecf5fa2024809
Bronson Oka <boka@slickage.com>
2014-11-11 16:32:42 -1000

    remove trailing spaces from index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var core = {};
 module.exports = function(dbPath) {
   // handle dbPath
   if (dbPath) { config.dbPath = dbPath; }
-  
+
   core.users = require(path.join(__dirname, 'users'));
   core.boards = require(path.join(__dirname, 'boards'));
   core.posts = require(path.join(__dirname, 'posts'));
