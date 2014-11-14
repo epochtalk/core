b3773e829cc348ba6c2b20f9d553df66ef44eaed
Bronson Oka <boka@slickage.com>
2014-11-13 14:10:19 -1000

    move boards/metadata decorator to helper

--- helper.js
+++ helper.js
@@ -6,5 +6,21 @@ module.exports = {
     timestamp = timestamp || Date.now();
     var id = timestamp + uuid.v1({ msecs: timestamp });
     return id;
+  },
+  decMetadata: function(storedModel) {
+    return new Promise(function(fulfill, reject) {
+      var model = storedModel.value;
+      tree.metadata({
+        key: storedModel.key,
+        callback: function(err, metadata) {
+          if (err) { reject(err); }
+          Object.keys(metadata).forEach(function(key) {
+            model[key] = metadata[key];
+          });
+          model.id = storedModel.key[1];
+          fulfill(model);
+        }
+      });
+    });
   }
 };
