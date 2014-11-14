e18362a375baa86efe5b8017e956f6c6ab6cbf3d
Bronson Oka <boka@slickage.com>
2014-11-13 17:22:00 -1000

    include user indexes

--- indexes.js
+++ indexes.js
@@ -1,4 +1,5 @@
 var path = require('path');
 var threadIndexes = require(path.join(__dirname, 'threads', 'indexes'));
 var postIndexes = require(path.join(__dirname, 'posts', 'indexes'));
-module.exports = [].concat(threadIndexes, postIndexes);
+var userIndexes = require(path.join(__dirname, 'users', 'indexes'));
+module.exports = [].concat(threadIndexes, postIndexes, userIndexes);
