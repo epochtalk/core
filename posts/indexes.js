a87cf22cb3fe0ef5881190541de818acf2bdbaf8
Bronson Oka <boka@slickage.com>
2014-12-01 11:28:25 -1000

    index post versions by updated_at

--- posts/indexes.js
+++ posts/indexes.js
@@ -1,4 +1,4 @@
 module.exports = [
   { type: 'post', parentType: 'thread', field: 'created_at' },
-  { type: 'postVersion', parentType: 'post', field: 'created_at' }
+  { type: 'postVersion', parentType: 'post', field: 'updated_at' }
 ];
