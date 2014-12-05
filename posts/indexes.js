98fe5553e70663b81056cd29fd764bd8e98f7f40
Bronson Oka <boka@slickage.com>
2014-12-04 17:46:49 -1000

    add agnostic index on post under smf.ID_MSG

--- posts/indexes.js
+++ posts/indexes.js
@@ -1,4 +1,5 @@
 module.exports = [
   { type: 'post', parentType: 'thread', field: 'created_at' },
+  { type: 'post', agnostic: true, field: 'smf.ID_MSG' },
   { type: 'postVersion', parentType: 'post', field: 'updated_at' }
 ];
