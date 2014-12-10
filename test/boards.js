227ed27f76a40c9866427c6e3673de58b42783b4
James Wang <jw@slickage.com>
2014-12-09 20:59:38 -1000

    Board create with parent/child test.

--- test/boards.js
+++ test/boards.js
@@ -23,13 +23,41 @@ function start() {
       name: 'Test Board',
       description: 'Test Board Description'
     };
-    boards.create(testBoard).
-    then(function(board) {
+    boards.create(testBoard).then(function(board) {
       t.ok(board, 'board returned from create is truthy');
       t.equal(testBoard.name, board.name, 'board name is correct');
       t.equal(testBoard.description, board.description, 'board description is correct');
       t.end();
-      teardown();
+    });
+  });
+  test('create parent/children', function(t) {
+    var parentBoard = { name: 'Parent Board', description: 'Description' };
+    var childABoard = { name: 'Child A Board', description: 'Description' };
+    var childBBoard = { name: 'Child B Board', description: 'Description' };
+    boards.create(parentBoard).then(function(board) {
+      parentBoard = board;
+      childABoard.parent_id = parentBoard.id;
+      childBBoard.parent_id = parentBoard.id;
+
+      return boards.create(childABoard)
+      .then(function(board) {
+        childABoard = board;
+      })
+      .then(function() {
+        return boards.create(childBBoard)
+        .then(function(board) {
+          childBBoard = board;
+        });
+      })
+      .then(function() {
+        return boards.find(parentBoard.id)
+        .then(function(board) {
+          t.equal(board.name, parentBoard.name, 'parent board name is correct');
+          t.equal(board.description, parentBoard.description, 'board description is correct');
+          t.end();
+          teardown();
+        });
+      });
     });
   });
 };
