713c8a9bcbfa45b229037c822f0721c163259d84
Bronson Oka <boka@slickage.com>
2014-12-11 16:10:02 -1000

    update thread to index agnostic on smf.ID_TOPIC

--- threads/indexes.js
+++ threads/indexes.js
@@ -1,5 +1,4 @@
-module.exports = {
-  type: 'thread',
-  parentType: 'board',
-  field: 'updated_at'
-};
+module.exports = [
+  { type: 'thread', agnostic: 'true', field: 'smf.ID_TOPIC' },
+  { type: 'thread', parentType: 'board', field: 'updated_at' }
+];
